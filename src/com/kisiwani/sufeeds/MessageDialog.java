5e0d2968efba0db26f1189fc084d692c881b3515
Salomon Metre <kulosmetros088@gmail.com>
2024-11-30 11:31:04 +0300

    add all the other files

---
+++ src/com/kisiwani/sufeeds/MessageDialog.java
@@ -0,0 +1,21 @@
+package com.kisiwani.sufeeds;
+
+import javax.swing.JOptionPane;
+
+public class MessageDialog {
+
+    // Static method to display a message in a JOptionPane
+    public static void showMessage(String text, String title) {
+        // Show message dialog with the provided text and title
+        JOptionPane.showMessageDialog(null, text, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void main(String[] args) {
+        // Example usage:
+        String message = "Hello, welcome to the system!";
+        String title = "Welcome Message";
+        
+        // Display the message in a JOptionPane
+        showMessage(message, title);
+    }
+}
---
+++ src/com/kisiwani/sufeeds/SUFeeds.java
@@ -0,0 +1,14 @@
+package com.kisiwani.sufeeds;
+
+
+public class SUFeeds {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainPage newMainPage = new MainPage();
+    }
+    
+}
